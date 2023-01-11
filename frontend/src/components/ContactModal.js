4ae8342945c097e8bbe785911a736e3586f0bc03
Gohar ul Islam <goharulislam@gmail.com>
2023-01-11 20:27:36 +0500

    Add files via upload

--- frontend/src/components/ContactModal.js
+++ frontend/src/components/ContactModal.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ContactModal(){
-return(
+const [isActive, setActive] = useState(true);
+let el1 = document.getElementById("open1");
+let el2 = document.getElementById("close1");
+let c1 = document.querySelector('.contact1');
 
-<div className="modal fade bottom-right" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div className="modal-dialog modal-fullscreen">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        ZINDA HAI GOHAR ZINDA HAI
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Understood</button>
-      </div>
-    </div>
+function ToggleClass(){
+console.log('gohar');
+
+if(isActive === true){
+  setActive(false);
+  el1.style.display = "none";
+  el2.style.display = "block";
+  c1.classList.remove('contact1');
+  c1.classList.add('contact2');
+}else{
+  setActive(true);
+  el1.style.display = "block";
+  el2.style.display = "none";
+  c1.classList.remove('contact2');
+  c1.classList.add('contact1');
+}
+}
+
+return(
+<div className={isActive ? "contact1" : "contact2"}>
+  <div id="open1" onClick={ToggleClass}><i className="fa fa-pencil"></i></div>
+  <div id="close1" onClick={ToggleClass}>
+    <section>
+    <div className="container">
+    <div className="row">
+    <div className="col-md-12">
+    <form>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="submit" value="SEND" />
+    </form>
+  </div>
+  </div>
+  </div>
+  </section>
+  <button><i className="fa fa-times"></i></button>
   </div>
 </div>
 
